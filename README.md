68f4114b9287e7965f88374e7f219dc727e18f30
mriandoe <ianpeysson@gmail.com>
2020-12-21 22:07:44 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# D-mineur
+## D-mineur
+
+#To do
+
+- Class Jeu
+x place et calcul huméro
+- découverte des espaces vide
+- ajout des flag
+- interface graphique
--- main.py
+++ main.py
@@ -1,33 +1,83 @@
 import random
+import copy
+
+class coordsErrorOutofrange(Exception):
+    pass
+
+class coordErrorMaskoff(Exception):
+    pass
+
+
+class Case:
+    def __init__(self,bombe=False):
+        self.bombe = bombe
+        self.mask = True
+        self.bombeadj = 0
+
+    def unmask(self):
+        if not self.mask:
+            raise coordErrorMaskoff
+
+        self.mask = False
+        return self.bombe
+
+    def affichage(self,DEBUG=False):
+        if(self.mask and not DEBUG):
+            print('*', end=" ")
+        elif(self.bombe):
+            print("B", end=" ")
+        else: 
+            print(self.bombeadj, end=" ")
 
 class Plateau:
-    def __init__(self, colonne, ligne):
+    
+    def __init__(self, colonne, ligne, pourcentage):
         self.c = colonne
         self.l = ligne
-        self.plateau = []
+        self.plateau = [[Case(random.randint(0,100) <= pourcentage) for j in range(self.c)] for i in range(self.l)]
+        self.nbombe = 0
 
-    def creation_plateau(self):
-        for i in range(self.l):
-            ligne = []
-            for j in range(self.c):
-                ligne.append('0')
-            self.plateau.append(ligne)
+        for x in range(self.l):
+            for y in range(self.c):
+                if self.plateau[x][y].bombe:
+                    self.nbombe += 1
+                    for dx in range(-1,2):
+                        for dy in range(-1,2):
+                            if (x+dx >= 0 and x+dx < self.l) and (y+dy >= 0 and y+dy < self.c):
+                                self.plateau[x+dx][y+dy].bombeadj += 1
+                            
 
-    def affichage_plateau(self):
+    def affichage(self,DEBUG=False):
         for ligne in self.plateau:
             for elem in ligne:
-                print(elem.ljust(3), end='')
+                elem.affichage(DEBUG)
             print()
 
-    def placer_mine(self):
-        posx = random.randint(0,self.l-1)
-        posy = random.randint(0, self.c-1)
-        self.plateau[posx][posy] = 'B'
+    def creuser(self,l,c):
+        if l > self.l or c > self.c:
+            raise coordsErrorOutofrange()
+        return self.plateau[l-1][c-1].unmask()
 
+def coord(p):
+    l = int(input("Ligne : "))
+    c = int(input("Colonne : "))
+    try : 
+        return p.creuser(l,c)
+    except coordsErrorOutofrange:
+        print("Coordonnées Trop grande")
+        return coord(p)
+    except coordErrorMaskoff:
+        print("Coordonnées déja rentrées")
+        return coord(p)
 
-p = Plateau(5, 5)
-p.creation_plateau()
-for i in range(5):
-    p.placer_mine()
+        
+
+p = Plateau(16,16, 10)
 #print(plateau)
-p.affichage_plateau()
+while True:
+    p.affichage(True)
+    print()
+    p.affichage()
+    if coord(p):
+        print("Ta perdu")
+        break
